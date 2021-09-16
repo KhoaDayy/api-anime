4c452fcd97c781722c9389d33c03fe23def1c133
Kynx <72400535+KhoaDayy@users.noreply.github.com>
2021-09-16 16:24:28 +0700

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const data = require('./MOCK_DATA.json');
 
 const app = express();
 
-app.use('/animegirl', (req, res) => {
+app.use(req, res) => {
         const {img} = data[Math.round(Math.random() * data.length)];
         const {author} = data[Math.round(Math.random() * data.length)];
         return res.json({ img, author });
-});
+};
 
 app.listen(4000, () => console.log('App listen at prot:4000'));
