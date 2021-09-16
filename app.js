161c018546a63302d5c688ad8bcb59fa43ca90d4
Kynx <72400535+KhoaDayy@users.noreply.github.com>
2021-09-16 16:11:43 +0700

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const data = require('./MOCK_DATA.json');
 
 const app = express();
 
-app.use('/random-image', (req, res) => {
+app.use('/animegirl', (req, res) => {
         const {img} = data[Math.round(Math.random() * data.length)];
         const {author} = data[Math.round(Math.random() * data.length)];
         return res.json({ img, author });
