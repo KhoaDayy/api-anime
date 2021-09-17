74a1e0277f188357aa948954e61ba72da4b10c75
Kynx <72400535+KhoaDayy@users.noreply.github.com>
2021-09-17 13:10:05 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
+//don't steal my code!!
 const express = require('express');
 const data = require('./MOCK_DATA.json');
 
 const app = express();
 
-app.use(req, res) => {
+app.use((req, res) => {
         const {img} = data[Math.round(Math.random() * data.length)];
         const {author} = data[Math.round(Math.random() * data.length)];
         return res.json({ img, author });
-};
+});
 
-app.listen(4000, () => console.log('App listen at prot:4000'));
+app.listen(4000, () => console.log('Sever is running'));
